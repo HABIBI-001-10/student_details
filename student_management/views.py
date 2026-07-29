7f48744e7a0fc3c11eb17275fb71f4a22641cf15
HABIBI-001-10 <habeebcva@gmail.com>
2026-07-29 10:14:41 +0530

    added crud operation delete,update,view,addis working properly

--- student_management/views.py
+++ student_management/views.py
@@ -4,6 +4,8 @@ from . models import Student
 
 # Create your views here.
 def add_student(request):
+
+
     if request.method == 'POST':
         # Process the form data and save the student
         name = request.POST.get('name')
@@ -22,34 +24,38 @@ def add_student(request):
     return render(request, 'add_student.html')
 
 def view_student(request, email=None):
+
     if request.method == 'POST':
         student_email = request.POST.get('email')
         return redirect('view_student_by_email', email=student_email)
     if email:
         student =get_object_or_404(Student, email=email )
         return render(request, 'view_student.html', {'student': student})
+    
     students = Student.objects.all()
+
     return render(request, 'view_student.html', {'students': students})
 
-def update_student(request):
+def update_student(request, id):
+    student = get_object_or_404(Student, id=id)
+
+
     if request.method == 'POST':
         # Process the form data and update the student
-        student_id = request.POST.get('student_id')
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        date_of_birth = request.POST.get('date_of_birth')
-        department = request.POST.get('department')
-
-        student = Student.objects.get(id=student_id)
-        student.name = name
-        student.email = email
-        student.date_of_birth = date_of_birth
-        student.department = department
+        student.name = request.POST.get('name')
+        student.email = request.POST.get('email')
+        student.date_of_birth = request.POST.get('date_of_birth')
+        student.department = request.POST.get('department')
         student.save()
 
-        return redirect('view_student')  # Redirect to the view_student page after updating
-    return render(request, 'update_student.html')
+        return redirect('view_student_by_email', email=student.email)  # Redirect to the view_student page after updating
+    return render(request, 'update_student.html', {'student': student})
 
-def delete_student(request):
-    return render(request, 'delete_student.html')
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
 
+    if request.method == 'POST':
+        student.delete()
+
+    return redirect('view_student')
+    
