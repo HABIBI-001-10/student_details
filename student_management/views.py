abe9175ef60d750a281a21b837bc06dd8abd427e
HABIBI-001-10 <habeebcva@gmail.com>
2026-07-30 23:00:11 +0530

    my 3rd commit

--- student_management/views.py
+++ student_management/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render ,get_list_or_404
 from django.http import HttpResponse
-from . models import Student
+from . forms import StudentForm
+from .models import Student
+
 
 # Create your views here.
 def home(request):
@@ -11,20 +13,18 @@ def add_student(request):
 
     if request.method == 'POST':
         # Process the form data and save the student
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        date_of_birth = request.POST.get('date_of_birth')
-        department = request.POST.get('department')
+        form = StudentForm(request.POST)
 
-        Student.objects.create(
-            name=name,
-            email=email,
-            date_of_birth=date_of_birth,
-            department=department
-        )
-        
-        return redirect('view_student')  # Redirect to the view_student page after adding
-    return render(request, 'add_student.html')
+        if form.is_valid():
+            form.save()
+            return redirect('view_student')  # Redirect to the view_student page after adding
+        else:
+            print(form.errors)  # Print form errors for debugging
+    else:
+        form = StudentForm()
+    return render(request, 'add_student.html', {'form': form})
+
+
 
 def view_student(request, email=None):
 
@@ -45,14 +45,14 @@ def update_student(request, id):
 
     if request.method == 'POST':
         # Process the form data and update the student
-        student.name = request.POST.get('name')
-        student.email = request.POST.get('email')
-        student.date_of_birth = request.POST.get('date_of_birth')
-        student.department = request.POST.get('department')
-        student.save()
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
 
         return redirect('view_student_by_email', email=student.email)  # Redirect to the view_student page after updating
-    return render(request, 'update_student.html', {'student': student})
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'update_student.html', {'form': form, 'student': student})
 
 def delete_student(request, id):
     student = get_object_or_404(Student, id=id)
